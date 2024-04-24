e123c384885147cfc612b967d0317c00a0ab4aca
nathan-websculpt <websculpt.nathan@gmail.com>
2024-04-24 10:59:42 -0500

    style tables

--- packages/nextjs/app/queries/page.tsx
+++ packages/nextjs/app/queries/page.tsx
@@ -5,14 +5,16 @@ import { MembersTable } from "~~/components/queries/membersTable/MembersTable";
 import { UsersTable } from "~~/components/queries/usersTable/UsersTable";
 
 const Queries: NextPage = () => {
-
-    return (
-      <>
+  return (
+    <>
+      <div className="container mx-auto my-10 overflow-auto">
         <UsersTable />
-
+      </div>
+      <div className="container mx-auto my-10 overflow-auto">
         <MembersTable />
-      </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Queries;
